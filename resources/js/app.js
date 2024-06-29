bb4b57badabf395c5af1e4d6c12ec53a45098632
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-29 12:46:07 +0200

    fix img

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,3 +31,29 @@ if(image) {
         };
     });
 }
+
+
+// colletction di array di bottoni delete
+const deleteSubmitButtons = document.querySelectorAll(".delete-button");
+// tramite il forEach prendiamo tutti i bottoni delete e aggiungiamo l'event listener
+deleteSubmitButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        // prendiamo il valore dell'attributo data-item-title
+        const dataTitle = button.getAttribute("data-item-title");
+        // prendiamo la modal
+        const modal = document.getElementById("deleteModal");
+
+        const bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+        // impostiamo il titolo della modale con il valore del data-item-title
+        const modalItemTitle = modal.querySelector("#modal-item-title");
+        modalItemTitle.textContent = dataTitle;
+        //prendiamo il bottone delete della modal
+        const buttonDelete = modal.querySelector("button.btn-primary");
+        // aggiungiamo l'event listener al bottone delete della modale
+        buttonDelete.addEventListener("click", () => {
+            button.parentElement.submit();
+        });
+    });
+});
