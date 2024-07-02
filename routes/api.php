8c61eaad34091d35beeeeb64949269092451b749
sashapoliti <sas.politi@gmail.com>
2024-07-02 15:05:01 +0200

    type controller api and add route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\RestaurantController;
+use App\Http\Controllers\API\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,7 @@ use App\Http\Controllers\API\RestaurantController;
 */
 
 Route::get('restaurants', [RestaurantController::class, 'index']);
+Route::get('types', [TypeController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
