f5cd6a833d6555fdae1b082c04f9c35df1eb09c9
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-07-02 15:03:28 +0200

    fix

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -100,7 +100,4 @@
             </div>
 
         </form>
-
-        <script src="//js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
-        <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
-    @endsection
+@endsection
