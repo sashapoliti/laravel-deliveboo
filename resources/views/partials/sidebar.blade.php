514f71ad597c02dbaff2aaaa8d5386fcad5a0d5c
sashapoliti <sas.politi@gmail.com>
2024-07-05 21:28:13 +0200

    fix logo sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <nav id="sidebar" class="navbar-dark">
     <div class="logo text-center" style="background-color: #f5f5f5">
         <a href="http://localhost:5174" class="nav-link text-uppercase overflow-hidden">
-            <img class="img-fluid rounded-circle" src="../img/logo2.png" alt="logo" style="width: 100px">
+            <img class="img-fluid rounded-circle" src="/img/logo2.png" alt="logo" style="width: 100px">
         </a>
     </div>
     <div class="user">
