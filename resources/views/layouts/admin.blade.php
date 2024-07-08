afe02d3c0c5b373db2976acdc0f4ecf80552e215
sashapoliti <sas.politi@gmail.com>
2024-07-08 12:34:07 +0200

    change title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Deliveboo - @yield('title')</title>
+    <title>Munchi - @yield('title')</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
