620ecf869b630397326dc48fd5f087ea82c20dd3
sashapoliti <sas.politi@gmail.com>
2024-07-09 14:36:14 +0200

    layout single order

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -4,33 +4,16 @@
 
 @section('content')
     <div class="container mt-5">
-        <h1 class="my-3">Ordine</h1>
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">Nome</th>
-                <th scope="col">Cognome</th>
-                <th scope="col">Email</th>
-                <th scope="col">Cellulare</th>
-                <th scope="col">Indirizzo</th>
-                <th scope="col">Totale</th>
-                <th scope="col">Data & ora</th>
-                <th scope="col">Azioni</th>
-              </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ $order->customer_name}}</td>
-                    <td>{{ $order->customer_surname }}</td>
-                    <td>{{ $order->customer_email }}</td>
-                    <td>{{ $order->customer_phone }}</td>
-                    <td>{{ $order->customer_address }}</td>
-                    <td>{{ $order->total_price }} €</td>
-                    <td>{{ $order->created_at->format('H:i') }} {{ $order->created_at->format('d/m/Y') }}</td>
-                    <td><a href="{{ route('admin.orders.index') }}">Indietro</a></td>
-                </tr>
-            </tbody>
-        </table>
+        <h1 class="my-3">Cliente</h1>
+        <ul>
+            <li><strong>Nome</strong>: {{ $order->customer_name }}</li>
+            <li><strong>Cognome</strong>: {{ $order->customer_surname }}</li>
+            <li><strong>Email</strong>: {{ $order->customer_email }}</li>
+            <li><strong>Cellulare</strong>: {{ $order->customer_phone }}</li>
+            <li><strong>Indirizzo</strong>: {{ $order->customer_address }}</li>
+            <li><strong>Totale</strong>: {{ $order->total_price }} €</li>
+            <li><strong>Data ordine</strong>: {{ $order->created_at->format('H:i') }} {{ $order->created_at->format('d/m/Y') }}</li>
+        </ul>
 
         <h2 class="my-3">Dettagli ordine</h2>
         <table class="table">
