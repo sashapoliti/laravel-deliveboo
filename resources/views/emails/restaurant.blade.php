db13b7af3e3ce78628d5268fdfe83ae6374ccecd
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-07-09 20:17:56 +0200

    fix restaurant order

--- resources/views/emails/restaurant.blade.php
+++ resources/views/emails/restaurant.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>🍽️ Nuovo Pagamento Ricevuto</title>
@@ -12,6 +13,7 @@
             margin: 0;
             padding: 0;
         }
+
         .container {
             max-width: 1200px;
             margin: 20px auto;
@@ -20,30 +22,37 @@
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
+
         h1 {
             color: #007BFF;
         }
+
         p {
             font-size: 1.1em;
         }
+
         .bold {
             font-weight: bold;
         }
+
         .order-details {
             margin-top: 20px;
         }
+
         .order-details ul {
             list-style-type: none;
             padding: 0;
         }
+
         .order-details li {
             margin-bottom: 10px;
         }
     </style>
 </head>
+
 <body>
     <div class="container">
-        <h1>🍽️ Nuovo Pagamento Ricevuto{{$restaurant_name}}</h1>
+        <h1>🍽️ Nuovo Pagamento Ricevuto{{ $restaurant_name }}</h1>
         <p>Gentile Ristorante,</p>
         <p>L'utente <strong>{{ $name }} {{ $surname }}</strong> ha effettuato un acquisto.</p>
         <p>Dettagli dell'ordine:</p>
@@ -58,9 +67,12 @@
                 <li><strong>ID Transazione:</strong> {{ $transactionId }}</li>
             </ul>
             <h2>🍲 Dettagli dei Piatti Ordinati:</h2>
+            @foreach ($cart as $item)
+                <li>{{ $item['product']['name'] }} - {{ $item['quantity'] }} x {{ $item['product']['price'] }} €</li>
+            @endforeach
         </div>
         <p>Grazie per la vostra attenzione.</p>
     </div>
 </body>
-</html>
 
+</html>
